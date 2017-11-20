ba7202ea21ad1674cc4043e049f169266f54c239
Andrii Kuz <ext-andrii.kuz@here.com>
2017-11-20 11:16:35 +0200

    Add API draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+use std::collections::LinkedList;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+pub struct MerkleTree<T: Hash> {
+    nodes: Vec<[u8; 64]>,
+    data: Vec<T>,
+}
+
+//add function verify_proof
+
+impl<T: Hash> MerkleTree<T> {
+    pub fn new(data: Vec<T>) -> MerkleTree<T> {
+        MerkleTree::generate_merkle_tree(data)
+    }
+
+    pub fn branch(&mut self, data: &T) {} // return vector of hashes
+    pub fn proof(&mut self, data: &T) {} // return vector of hashes
+
+    fn generate_merkle_tree(data: Vec<T>) -> MerkleTree<T> {
+        MerkleTree {
+            nodes: Vec::new(),
+            data,
+        }
+    }
+
+    // add push leaf function
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
