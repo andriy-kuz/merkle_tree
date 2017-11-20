a5ef464a4f077dcb50045564474809cb0b049244
Andrii Kuz <ext-andrii.kuz@here.com>
2017-11-20 20:03:28 +0200

    Transit commit - removing data dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,36 @@ extern crate hex;
 extern crate openssl;
 
 use openssl::sha;
-// Also could be used std::hash trait
+
 pub trait SHA256Hash {
     fn hash(&self) -> [u8; 32];
 }
 
-pub struct MerkleTree<T: SHA256Hash> {
-    nodes: Vec<[u8; 32]>,
-    data: Vec<T>,
+// add push leaf function
+// to support this add is_odd flag
+pub struct MerkleTree {
+    tree: Vec<[u8; 32]>,
+    leafs_count: usize,
 }
 
-impl<T: SHA256Hash> MerkleTree<T> {
-    pub fn new(data: Vec<T>) -> MerkleTree<T> {
-        MerkleTree::generate_merkle_tree(data)
+impl MerkleTree {
+    pub fn new(leafs: Vec<[u8; 32]>) -> MerkleTree {
+        MerkleTree::generate_merkle_tree(leafs)
+    }
+
+    pub fn new_from_data<T: SHA256Hash>(data: &Vec<T>) -> MerkleTree {
+        let leafs = Vec::new();
+        leafs.reserve(data.len());
+
+        for val in data {
+            leafs.push(val.hash());
+        }
+        MerkleTree::generate_merkle_tree(leafs)
     }
 
     // last element - root value
-    pub fn branch(&self, data: &T) -> Vec<[u8; 32]> {
-        let hash = data.hash();
-        let index = self.nodes.iter().position(|&x| x == hash);
+    pub fn branch(&self, hash: &[u8; 32]) -> Vec<[u8; 32]> {
+        let index = self.tree.iter().position(|&x| x == *hash);
 
         if let Some(mut index) = index {
             let mut result = Vec::new();
@@ -35,7 +46,7 @@ impl<T: SHA256Hash> MerkleTree<T> {
         Vec::new()
     }
     // last element of vector on top in tree
-    pub fn proof(&self, data: &T) -> Vec<[u8; 32]> {
+    pub fn proof(&self, data: &[u8; 32]) -> Vec<[u8; 32]> {
         let hash = data.hash();
         let index = self.nodes.iter().position(|&x| x == hash);
 
@@ -53,19 +64,14 @@ impl<T: SHA256Hash> MerkleTree<T> {
         Vec::new()
     }
 
-    fn generate_merkle_tree(data: Vec<T>) -> MerkleTree<T> {
-        let nodes_count = 2 * data.len() - 1;
+    fn generate_merkle_tree(leafs: Vec<[u8; 32]>) -> MerkleTree {
+        let leafs_count = leafs.len();
+        let nodes_count = 2 * leafs_count - 1;
         let mut nodes: Vec<[u8; 32]> = Vec::with_capacity(nodes_count);
-        nodes.resize(nodes_count, [0; 32]);
+        // in performance view
+        nodes.resize(nodes_count - leafs_count, [0; 32]);
         //add leafs hashes
-        {
-            let mut index = nodes_count - data.len();
-
-            for value in &data {
-                nodes[index] = value.hash();
-                index += 1;
-            }
-        }
+        nodes.append(&mut leafs);
         //create tree
         {
             let mut index = nodes.len() - 1;
@@ -75,8 +81,10 @@ impl<T: SHA256Hash> MerkleTree<T> {
                 index -= 2;
             }
         }
-
-        MerkleTree { nodes, data }
+        MerkleTree {
+            tree: nodes,
+            leafs_count,
+        }
     }
 
     fn get_brother(index: usize) -> usize {
@@ -89,7 +97,6 @@ impl<T: SHA256Hash> MerkleTree<T> {
         let index = lh.max(rh);
         index / 2 - 1
     }
-    // add push leaf function
 }
 
 fn get_hash(lh: &[u8; 32], rh: &[u8; 32]) -> [u8; 32] {
